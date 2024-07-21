9011db9d7641b8fdb7166acff0643177122fd0d6
PritRaj1 <pr478@cam.ac.uk>
2024-07-21 01:28:46 +0000

    grid update implemented

--- src/architecture/kan_layer.jl
+++ src/architecture/kan_layer.jl
@@ -9,7 +9,7 @@ include("../utils.jl")
 using .Spline: extend_grid, coef2curve, curve2coef
 using .Utils: device, sparse_mask
 
-struct kan_dense
+mutable struct kan_dense
     in_dim::Int
     out_dim::Int
     num_splines::Int
@@ -49,11 +49,13 @@ end
 Flux.@functor kan_dense (grid, coef, σ_base, σ_sp, mask)
 
 function (l::kan_dense)(x)
-
     b_size = size(x, 1)
+
+    # Base activation.
     pre_acts = repeat(reshape(copy(x), b_size, 1, l.in_dim), 1, l.out_dim, 1)
     base = l.base_act(x)
 
+    # B-spline basis functions of degree k
     y = coef2curve(x, l.grid, l.coef; k=l.degree)
     post_spline = permutedims(copy(y), [1, 3, 2])
 
@@ -66,17 +68,43 @@ function (l::kan_dense)(x)
     return y, pre_acts, post_acts, post_spline
 end
 
-function update_grid!(l::kan_dense, x, y)
+function update_grid!(l::kan_dense, x; margin=0.01)
+    """
+    Adapt the grid to the distribution of the input data
+
+    Args:
+        l: The KAN layer.
+        x: A matrix of size (b, d) containing the input data.
+        y: A matrix of size (b, l) containing the target data.
+        margin: The margin to add to the grid.
+    """
     b_size = size(x, 1)
-    x_pos, _ = sort(x, dims=1)
+    
+    # Compute the B-spline basis functions of degree k
+    x_pos = sortslices(x, dims=1)
     y_eval = coef2curve(x_pos, l.grid, l.coef; k=l.degree)
+
     num_interval = size(l.grid, 2) - 2*l.degree - 1
-    ids = [Int(batch / num_interval * i) for i in 0:num_interval-1]  
-    ids = [ids...]  
-    ids = vcat(ids, -1)
+    ids = [div(b_size * i, num_interval) for i in 1:num_interval]
+    grid_adaptive = zeros(0, size(x, 2)) |> device
+    for idx in ids
+        grid_adaptive = vcat(grid_adaptive, x_pos[idx:idx, :])
+    end
+    grid_adaptive = vcat(grid_adaptive, x_pos[end:end, :])
+    grid_adaptive = grid_adaptive |> permutedims 
+
+    h = (grid_adaptive[:, end:end] .- grid_adaptive[:, 1:1]) ./ num_interval # step size
+    range = device(collect(0:num_interval))[:, :] |> permutedims 
+    grid_uniform = h .* range .+ grid_adaptive[:, 1:1] 
+    grid = @tullio out[i, j] := l.grid_eps * grid_uniform[i, j] + (1 - l.grid_eps) * grid_adaptive[i, j]
+
+    l.grid = extend_grid(grid, l.degree)
+    l.coef = curve2coef(x_pos, y_eval, l.grid; k=l.degree)
 end
 
 # Test the KAN layer
 model = dense_layer(3, 5) |> device
 x = randn(100, 3) |> device
-y = model(x)
+y = randn(100, 5) |> device
+z = model(x)
+update_grid!(model, x)
--- src/architecture/spline.jl
+++ src/architecture/spline.jl
@@ -35,38 +35,40 @@ function B_batch(x, grid; degree::Int64, eps=1e-6)
     Compute the B-spline basis functions for a batch of points x and a grid of knots.
 
     Args:
-        x: A matrix of size (d, n) where d is the number of splines and n is the number of samples.
-        grid: A matrix of size (d, m) where d is the number of splines and m is the number of evaluation points.
-        k: The degree of the B-spline basis functions.
-        extend: If true, extend the grid of knots to include the boundary knots.
-    
+        x: A matrix of size (d, n) containing the points at which to evaluate the B-spline basis functions.
+        grid: A matrix of size (d, m) containing the grid of knots.
+        degree: The degree of the B-spline basis functions.
+
     Returns:
-        A matrix of size (d, p, n) containing the 'p' B-spline basis coefficients evaluated at the points x.
+        A matrix of size (d, m, n) containing the B-spline basis functions evaluated at the points x.
     """
 
     d, m = size(grid)
     n = size(x, 2)
+    
+    # B-spline basis functions of degree 0 are piecewise constant functions: B = 1 if x in [grid[p], grid[p+1]) else 0
+    grid_1 = grid[:, 1:end-1] # grid[p]
+    grid_2 = grid[:, 2:end] # grid[p+1]
+
+    term1 = @tullio term1[i, j, k] := (x[i, j] >= grid_1[j, k] ? 1.0 : 0.0)
+    term2 = @tullio term2[i, j, k] := (x[i, j] < grid_2[j, k] ? 1.0 : 0.0)
+    term1 |> collect |> device
+    term2 |> collect |> device
+
+    B = @tullio res[d, p, n] := term1[d, p, n] * term2[d, p, n]
+
     x = reshape(x, size(x)..., 1) 
     grid = reshape(grid, 1, size(grid)...) 
 
-    # B-spline basis functions of degree 0 are piecewise constant functions: B = 1 if x in [grid[p], grid[p+1]) else 0
-
-    grid_1 = grid[:, :, 1:end-1] # grid[p] # expand you bitch
-    grid_2 = grid[:, :, 2:end] # grid[p+1]
-
-    term1 = @tullio term1[d, n, m] := (x[d, n, 1] >= grid_1[1, d, m] ? 1.0 : 0.0) 
-    term2 = @tullio term2[d, n, m] := (x[d, n, 1] < grid_2[1, d, m] ? 1.0 : 0.0)
-    B = @tullio res[d, n, m] := term1[d, n, m] * term2[d, n, m]
-
-    for k in 1:degree
-        # Compute the B-spline basis functions of degree k:
+    # Compute the B-spline basis functions of degree k
+    for k in 1:degree 
         numer1 = x .- grid[:, :, 1:(end - k - 1)]
         denom1 = grid[:, :, (k + 1):end-1] .- grid[:, :, 1:(end - k - 1)]
         numer2 = grid[:, :, (k + 2):end] .- x
         denom2 = grid[:, :, (k + 2):end] .- grid[:, :, 2:(end - k)]
         B_i1 = B[:, :, 1:end - 1]
         B_i2 = B[:, :, 2:end]
-        # out = numer1 ./ denom1 .* B[:, :, 1:end - 1] .+ numer2 ./ denom2 .* B[:, :, 2:end]
+        B = @tullio out[d, n, m] := (numer1[d, n, m] / denom1[1, n, m] * B_i1[d, n, m]) + (numer2[d, n, m] / denom2[1, n, m] * B_i2[d, n, m])
     end
 
     replace!(B, NaN=>eps)
@@ -78,15 +80,15 @@ function coef2curve(x_eval, grid, coef; k::Int64)
     Compute the B-spline curves from the B-spline coefficients.
 
     Args:
-        x_eval: A matrix of size (d, n) where d is the number of splines and n is the number of samples.
-        grid: A matrix of size (d, m) where d is the number of splines and m is the number of grid points.
-        coef: A matrix of size (d, p) where d is the number of splines and p is the number of coefficients.
+        x_eval: A matrix of size (d, n) containing the points at which to evaluate the B-spline curves.
+        grid: A matrix of size (d, m) containing the grid of knots.
+        coef: A matrix of size (d, m, l, k) containing the B-spline coefficients.
         k: The degree of the B-spline basis functions.
 
     Returns:
-        A matrix of size (d, n) containing the B-spline curves evaluated at the points x_eval.
+        A matrix of size (d, l, n) containing the B-spline curves evaluated at the points x_eval.
     """
-    b_splines = B_batch(x_eval, grid; k) 
+    b_splines = B_batch(x_eval, grid; degree=k) 
     y_eval = @tullio out[i, j, l] := b_splines[i, j, k] * coef[j, l, k]
     return y_eval
 end
@@ -96,24 +98,26 @@ function curve2coef(x_eval, y_eval, grid; k::Int64, eps=1e-6)
     Convert B-spline curves to B-spline coefficients using least squares.
 
     Args:
-        x_eval: A matrix of size (d, n) where d is the number of splines and n is the number of samples.
-        y_eval: A matrix of size (d, n) where d is the number of splines and n is the number of samples.
-        grid: A matrix of size (d, m) where d is the number of splines and m is the number of grid points.
-        k: The piecewise polynomial order of splines.
+        x_eval: A matrix of size (d, n) containing the points at which the B-spline curves were evaluated.
+        y_eval: A matrix of size (d, l, n) containing the B-spline curves evaluated at the points x_eval.
+        grid: A matrix of size (d, m) containing the grid of knots.
+        k: The degree of the B-spline basis functions.
 
     Returns:
-        A matrix of size (d, p) containing the B-spline coefficients.
+        A matrix of size (d, m, l, k) containing the B-spline coefficients.
     """
     n_coeffs = size(grid, 2) - k - 1
     out_dim = size(y_eval, 3)
-    B = B_batch(x_eval, grid; k) 
+
+    B = B_batch(x_eval, grid; degree=k) 
     B = permutedims(B, [2, 1, 3])
     B = reshape(B, size(B, 1), 1, size(B, 2), size(B, 3))
     B = repeat(B, 1, out_dim, 1, 1) 
+    
     y_eval = permutedims(y_eval, [2, 3, 1])
     y_eval = reshape(y_eval, size(y_eval)..., 1) 
 
-    # Least squares solution
+    # Least squares solution, (A'A) * coef = A'y
     denom = replace(sum(B .* B, dims=3), 0.0=>eps)
     coef = sum(B .* y_eval, dims=3) ./ denom
 

32331574b7369f18aa1b3e900b6b14e93bc206e0
Prithvi <pr478@cam.ac.uk>
2024-07-22 01:07:41 +0100

    implemented symbolic kan layer

--- src/unit_tests/test_layer.jl
+++ src/unit_tests/test_layer.jl
@@ -1,8 +1,11 @@
 include("../architecture/kan_layer.jl")
+include("../architecture/symbolic_layer.jl")
 
 using Test
 using .dense_kan
+using .symbolic_layer
 
+# Test b_spline_layer
 layer = b_spline_layer(3, 5)
 x = randn(100, 3) 
 y = randn(100, 3) 
@@ -17,3 +20,10 @@ z, preacts, postacts, postspline = layer(x)
 update_grid!(layer, x)
 
 @test all(size(layer.grid) .== (3, 12))
+
+# Test symbolic_dense
+layer = symbolic_dense(3, 5)
+z, postacts = layer(x)
+
+@test all(size(z) .== (100, 5))
+@test all(size(postacts) .== (100, 5, 3))

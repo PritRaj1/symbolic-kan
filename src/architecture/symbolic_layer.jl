9e3d087337c255dd92ef607320619c812fefa445
Prithvi <pr478@cam.ac.uk>
2024-07-22 02:05:35 +0100

    implemented subset extraction for pruning

--- src/architecture/symbolic_layer.jl
+++ src/architecture/symbolic_layer.jl
@@ -1,15 +1,15 @@
 module symbolic_layer
 
-export symbolic_kan_kayer
+export symbolic_kan_layer, get_subset
 
 using Flux, CUDA, KernelAbstractions, Tullio
 
 include("../symbolic_lib.jl")
 include("../utils.jl")
 using .SymbolicLib: SYMBOLIC_LIB
-using .Utils
+using .Utils: device
 
-struct symbolic_dense
+mutable struct symbolic_dense
     in_dim::Int
     out_dim::Int
     mask
@@ -20,7 +20,7 @@ struct symbolic_dense
     affine
 end
 
-function symbolic_kan_kayer(in_dim::Int, out_dim::Int)
+function symbolic_kan_layer(in_dim::Int, out_dim::Int)
     mask = ones(out_dim, in_dim)
     fcns = [[x -> 0.0 for i in 1:in_dim] for j in 1:out_dim] 
     fcns_avoid_singular = [[(x, y_th) -> ((), x * 0.0) for i in 1:in_dim] for j in 1:out_dim]
@@ -34,12 +34,28 @@ end
 Flux.@functor symbolic_dense (mask, affine)
 
 function (l::symbolic_dense)(x; avoid_singular=false, y_th=10.0)
+    """
+    Apply symbolic dense layer to input x using Kolmogorov-Arnold theorm.
+    
+    i.e apply reference univariate functions to each element of multi-dim sample,
+        then sum along input dimension.
+
+    Args:
+    - x: input tensor of shape (batch_size, in_dim).
+    - avoid_singular: whether to avoid singularities.
+    - y_th: threshold for singularities.
+
+    Returns:
+    - z: output tensor of shape (batch_size, out_dim).
+    - post_acts: post activation tensor of shape (batch_size, out_dim, in_dim).
+    """
+
     b_size = size(x, 1)
     avoid_singular ? y_th = repeat([10.0], b_size, 1) |> device : y_th = nothing
 
-    post_acts = zeros(b_size, l.out_dim, 0)
+    post_acts = zeros(b_size, l.out_dim, 0) |> device
     for i in 1:l.in_dim
-        post_acts_ = zeros(b_size, 0)
+        post_acts_ = zeros(b_size, 0) |> device
         for j in 1:l.out_dim
             if avoid_singular
                 f_xy = l.fcns_avoid_singular[j][i].(l.affine[j, i, 1] .* x[:, i:i] .+ l.affine[j, i, 2], y_th)[2]
@@ -55,6 +71,41 @@ function (l::symbolic_dense)(x; avoid_singular=false, y_th=10.0)
     end
 
     return sum(post_acts, dims=3)[:, :, 1], post_acts
+end
+
+function get_subset(l::symbolic_dense, in_indices, out_indices)
+    """
+    Extract smaller symbolic dense layer from larger layer for pruning.
+    
+    Args:
+    - l: symbolic dense layer.
+    - in_indices: indices of input dimensions to keep.
+    - out_indices: indices of output dimensions to keep.
+
+    Returns:
+    - l_sub: subset of symbolic dense layer.
+    """
+    l_sub = symbolic_kan_layer(l.in_dim, l.out_dim)
+
+    l_sub.in_dim, l_sub.out_dim = length(in_indices), length(out_indices)
+    
+    new_mask = zeros(l_sub.out_dim, 0) |> device
+    for i in in_indices
+        new_mask_ = zeros(0, 1) |> device
+            for j in out_indices
+                new_mask_ = vcat(new_mask_, l.mask[j:j, i:i])
+            end
+        new_mask = hcat(new_mask, new_mask_)
+    end
+
+    l_sub.mask = new_mask
+    l_sub.fcns = [[l.fcns[j][i] for i in in_indices] for j in out_indices]
+    l_sub.fcns_avoid_singular = [[l.fcns_avoid_singular[j][i] for i in in_indices] for j in out_indices]
+    l_sub.fcn_names = [[l.fcn_names[j][i] for i in in_indices] for j in out_indices]
+    l_sub.fcn_sympys = [[l.fcn_sympys[j][i] for i in in_indices] for j in out_indices]
+    l_sub.affine = l.affine[out_indices, in_indices, :]
+
+    return l_sub
 end
 
 end
--- src/unit_tests/test_layer.jl
+++ src/unit_tests/test_layer.jl
@@ -6,24 +6,39 @@ using .dense_kan
 using .symbolic_layer
 
 # Test b_spline_layer
-layer = b_spline_layer(3, 5)
-x = randn(100, 3) 
-y = randn(100, 3) 
-z, preacts, postacts, postspline = layer(x)
+function test_spline_lyr()
+    layer = b_spline_layer(3, 5)
+    x = randn(100, 3) 
+    y = randn(100, 3) 
+    z, preacts, postacts, postspline = layer(x)
 
-@test all(size(z) .== (100, 5))
-@test all(size(preacts) .== (100, 5, 3))
-@test all(size(postacts) .== (100, 5, 3))
-@test all(size(postspline) .== (100, 5, 3))
-@test all(size(layer.grid) .== (3, 12))
+    @test all(size(z) .== (100, 5))
+    @test all(size(preacts) .== (100, 5, 3))
+    @test all(size(postacts) .== (100, 5, 3))
+    @test all(size(postspline) .== (100, 5, 3))
+    @test all(size(layer.grid) .== (3, 12))
 
-update_grid!(layer, x)
+    update_grid!(layer, x)
 
-@test all(size(layer.grid) .== (3, 12))
+    @test all(size(layer.grid) .== (3, 12))
+end
 
 # Test symbolic_dense
-layer = symbolic_dense(3, 5)
-z, postacts = layer(x)
+function test_symb_lyr()
+    layer = symbolic_kan_layer(3, 5)
+    x = randn(100, 3) 
+    z, postacts = layer(x)
 
-@test all(size(z) .== (100, 5))
-@test all(size(postacts) .== (100, 5, 3))
+    @test all(size(z) .== (100, 5))
+    @test all(size(postacts) .== (100, 5, 3))
+
+    layer = symbolic_kan_layer(10, 10)
+    layer = get_subset(layer, [1,10],[2,3,4])
+
+    @test all(size(layer.mask) .== (3, 2))
+    @test layer.in_dim == 2
+    @test layer.out_dim == 3
+end
+
+test_spline_lyr()
+test_symb_lyr()
